f391488db5cbff12f3da253b604060bd2e8a80d5
Tammo Jan Dijkema <T.J.Dijkema@gmail.com>
2016-04-18 19:55:52 +0200

    Update lin2circ.py, closes #5

--- Maintained/lin2circ.py
+++ Maintained/lin2circ.py
@@ -1,12 +1,5 @@
 #!/usr/bin/env python
 
-#
-# Written by George Heald, heald@astron.nl, November 2010, Version 1.0
-#
-
-
-
-
 """
 lin2circ.py
 Converts linear XX,XY,YX,YY correlations to circular RR,RL,LR,LL
@@ -39,6 +32,25 @@ LR = XX - iXY - iYX - YY
 LL = XX + iXY - iYX + YY
 
 Version 1.0 written 15 April 2010 by George Heald
+
+Version 1.1 (18 March 2012) adds update of POLARIZATION table
+
+Version 1.2 (July 6, 2012) - added inverse capability, convert from circular
+to linear polarization.
+
+Version 1.3 (2014, Reinout van Weeren) - corrected the flux error, factor of 0.5 (/2) was missing. Added option to convert back from circular to linear
+
+Since the transformation matrices are hermitian, C_A ^-1 = C+_A and C+_A^-1 = C_A.
+So, we have: 
+
+	V_XY = C+_A*V_RL*C_A
+	
+which is:
+
+XX =   RR +  RL +  LR +  LL
+XY =  iRR - iRL + iLR - iLL
+YX = -iRR - iRL + iLR + iLL
+YY =   RR -  RL -  LR +  LL
 """
 
 import optparse
@@ -47,38 +59,77 @@ import numpy
 
 def main(options):
 
-        cI = numpy.complex(0.,1.)
+	cI = numpy.complex(0.,1.)
+	
+	inms = options.inms
+	if inms == '':
+			print 'Error: you have to specify an input MS, use -h for help'
+			return
+	column = options.column
+	outcol = options.outcol
+	
+	t = pt.table(inms, readonly=False, ack=True)
+	if options.back:
+		lincol = options.lincol
+		if lincol not in t.colnames():
+				print 'Adding the output linear polarization column',lincol,'to',inms
+				coldmi = t.getdminfo(column)
+				coldmi['NAME'] = lincol
+				t.addcols(pt.maketabdesc(pt.makearrcoldesc(lincol, 0., valuetype='complex', shape=numpy.array(t.getcell(column,0)).shape)), coldmi)
 
-        inms = options.inms
-        if inms == '':
-                print 'Error: you have to specify an input MS, use -h for help'
-                return
-        column = options.column
-        outcol = options.outcol
-
-        t = pt.table(inms, readonly=False, ack=True)
-        if outcol not in t.colnames():
-                print 'Adding output column',outcol,'to',inms
-                coldmi = t.getdminfo(column)
-                coldmi['NAME'] = outcol
-                t.addcols(pt.maketabdesc(pt.makearrcoldesc(outcol, 0., valuetype='complex', shape=numpy.array(t.getcell(column,0)).shape)), coldmi)
-        print 'Reading input column'
-        data = t.getcol(column)
-        print 'Computing output column'
-        outdata = numpy.transpose(numpy.array([
-                data[:,:,0]-cI*data[:,:,1]+cI*data[:,:,2]+data[:,:,3],
-                data[:,:,0]+cI*data[:,:,1]+cI*data[:,:,2]-data[:,:,3],
-                data[:,:,0]-cI*data[:,:,1]-cI*data[:,:,2]-data[:,:,3],
-                data[:,:,0]+cI*data[:,:,1]-cI*data[:,:,2]+data[:,:,3]]),
-                (1,2,0))
-        print 'Finishing up'
-        t.putcol(outcol, outdata)
-
+                ### RVW EDIT 2012   
+		print 'Reading the input column (circular)', column
+		if column not in t.colnames():
+			print 'Error: Input column does not exist'
+			return
+		
+		### RVW EDIT 2012 Input column with the -c switch 
+		cirdata = t.getcol(column)
+		#cirdata = t.getcol(outcol)
+		#print 'SHAPE ARRAY', numpy.shape(cirdata)
+		
+		print 'Computing the linear polarization terms...'
+		lindata = numpy.transpose(numpy.array([
+				0.5*(cirdata[:,:,0]+cirdata[:,:,1]+cirdata[:,:,2]+cirdata[:,:,3]),
+				0.5*(cI*cirdata[:,:,0]-cI*cirdata[:,:,1]+cI*cirdata[:,:,2]-cI*cirdata[:,:,3]),
+				0.5*(-cI*cirdata[:,:,0]-cI*cirdata[:,:,1]+cI*cirdata[:,:,2]+cI*cirdata[:,:,3]),
+				0.5*(cirdata[:,:,0]-cirdata[:,:,1]-cirdata[:,:,2]+cirdata[:,:,3])]),
+				(1,2,0))
+		print 'Finishing up...'
+		t.putcol(lincol, lindata)
+	else:
+		if outcol not in t.colnames():
+			print 'Adding the output column',outcol,'to',inms
+			coldmi = t.getdminfo(column)
+			coldmi['NAME'] = outcol
+			t.addcols(pt.maketabdesc(pt.makearrcoldesc(outcol, 0., valuetype='complex', shape=numpy.array(t.getcell(column,0)).shape)), coldmi)
+		print 'Reading the input column (linear)', column
+		data = t.getcol(column)
+		print 'Computing the output column'
+		outdata = numpy.transpose(numpy.array([
+				0.5*(data[:,:,0]-cI*data[:,:,1]+cI*data[:,:,2]+data[:,:,3]),
+				0.5*(data[:,:,0]+cI*data[:,:,1]+cI*data[:,:,2]-data[:,:,3]),
+				0.5*(data[:,:,0]-cI*data[:,:,1]-cI*data[:,:,2]-data[:,:,3]),
+				0.5*(data[:,:,0]+cI*data[:,:,1]-cI*data[:,:,2]+data[:,:,3])]),
+				(1,2,0))
+		print 'Finishing up...'
+		t.putcol(outcol, outdata)
+	if options.poltable:
+		print 'Updating the POLARIZATION table...'
+		tp = pt.table(inms+'/POLARIZATION',readonly=False,ack=True)
+		
+		### RVW EDIT 2012
+		if options.back:
+		   tp.putcol('CORR_TYPE',numpy.array([[9,10,11,12]],dtype=numpy.int32)) # FROM CIRC-->LIN
+                else:
+		   tp.putcol('CORR_TYPE',numpy.array([[5,6,7,8]],dtype=numpy.int32)) # FROM LIN-->CIRC
 
 opt = optparse.OptionParser()
 opt.add_option('-i','--inms',help='Input MS [no default]',default='')
 opt.add_option('-c','--column',help='Input column [default DATA]',default='DATA')
 opt.add_option('-o','--outcol',help='Output column [default DATA_CIRC]',default='DATA_CIRC')
+opt.add_option('-p','--poltable',help='Update POLARIZATION table? [default False]',default=False,action='store_true')
+opt.add_option('-b','--back',help='Go back to linear polarization [default False]',default=False,action='store_true')
+opt.add_option('-l','--lincol',help='Output linear polarization column, if the -b switch is used [default DATA_LIN]; we want to keep the original DATA column',default='DATA_LIN')
 options, arguments = opt.parse_args()
 main(options)
-
